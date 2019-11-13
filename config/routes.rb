5f5196ec3a3bf822a9b87126e6fd446026b86ecd
leonard percival <leonardpercival@gmail.com>
2019-11-13 10:55:58 +0000

    morning lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
+
+
+  resources :restaurants do
+
+    #  NESTED RESOURCE
+    resources :reviews, only: [ :new, :create ]
+
+    # STATIC
+    collection do                       # collection => no restaurant id in URL
+      get 'top'
+    end
+
+    # DYNAMIC
+    member do
+      get 'chef'
+    end
+  end
+
+  resources :reviews, only: [:edit, :update, :destroy, :show]
+
 end
